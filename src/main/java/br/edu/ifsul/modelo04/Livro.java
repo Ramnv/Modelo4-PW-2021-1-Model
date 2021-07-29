5cd560c4f1503e8c6010506c661012d76c2b6cc3
Ramon <ramonmaito@gmail.com>
2021-07-29 09:22:04 -0300

    Inicio

---
+++ src/main/java/br/edu/ifsul/modelo04/Livro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.modelo04;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author rvelasco
+ */
+public class Livro {
+
+    String codigoBarras;
+    Integer numeroPaginas;
+    Boolean ativo;
+    Calendar dataCadastro;
+    Double valor;
+}
---
+++ src/main/java/br/edu/ifsul/modelo04/LivroBasico.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.modelo04;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author rvelasco
+ */
+public class LivroBasico {
+
+    String ISBN;
+    String titulo;
+    String resumo;
+    String editora;
+    Calendar dataPublicacao;
+}
