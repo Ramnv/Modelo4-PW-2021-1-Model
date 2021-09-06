b28fcd68649bcac7eed591f466bfe7740b82e1e8
Ramon <ramonmaito@gmail.com>
2021-09-06 14:17:46 -0300

    relatorio formatos

--- src/main/java/br/edu/ifsul/util/relatorios/FabricaObjetos.java
+++ src/main/java/br/edu/ifsul/util/relatorios/FabricaObjetos.java
@@ -1,6 +1,7 @@
 package br.edu.ifsul.util.relatorios;
 
-
+import br.edu.ifsul.modelo04.Autor;
+import br.edu.ifsul.modelo04.Formato;
 import br.edu.ifsul.modelo04.Idioma;
 import br.edu.ifsul.modelo04.Livro;
 import java.util.ArrayList;
@@ -35,12 +36,46 @@ public class FabricaObjetos {
         lista.add(t);
         return lista;
     }
-    
-//    public static List<Livro> carregaLivros() {
-//        List<Livro> lista = new ArrayList<>();
-//        Livro l = new Livro();
-//
-//        lista.add(l);
-//        return lista;
-//    }
+
+    public static List<Formato> carregaFormatos() {
+        List<Formato> lista = new ArrayList<>();
+        Formato form = new Formato();
+        form.setId(Integer.SIZE);
+        form.setNome("EPUB");
+        lista.add(form);
+        return lista;
+    }
+
+    public static List<Livro> carregaLivros() {
+        List<Livro> lista = new ArrayList<>();
+        Livro l = new Livro();
+        l.setISBN("abc1234567890");
+        l.setTitulo("tituloWEB");
+        l.setResumo("asdfjlçksafsadçjflasasdfjlçksafsadçjflas asdfjlçksafsadçjflas asdfjlçksafsadçjflas asdfjlçksafsadçjflas");
+        l.setEditora("Editora Saraiva");
+        l.setDataPublicacao(Calendar.getInstance());
+        l.setCodigoBarras("123456789524");
+        l.setNumeroPaginas(300);
+        l.setAtivo(true);
+        l.setDataCadastro(Calendar.getInstance());
+        l.setValor(100.00);
+
+        Autor aut = new Autor();
+        aut.setNome("Stephen");
+        aut.setBibliografia("Bio King");
+
+        Idioma idi = new Idioma();
+        idi.setNome("Inglês");
+        idi.setSigla("EN");
+
+        Formato form = new Formato();
+        form.setNome("MOBI");
+
+//        l.setAutores(aut);
+        l.setIdioma(idi);
+        l.setFormato(form);
+
+        lista.add(l);
+        return lista;
+    }
 }
